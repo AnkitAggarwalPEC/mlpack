d16a7cf1aed7669b337ca159d70894648e9330c3
ankit.a <ankit.a@irage.in>
2017-06-22 17:09:05 +0530

    Change for the traverse

--- src/mlpack/methods/kcentre/gonzalez_kcentre_impl.hpp
+++ src/mlpack/methods/kcentre/gonzalez_kcentre_impl.hpp
@@ -5,6 +5,7 @@
 #ifndef MLPACK_METHODS_KCENTRE_GONZALEZ_IMPL_HPP
 #define MLPACK_METHODS_KCENTRE_GONZALEZ_IMPL_HPP
 #include "gonzalez_kcentre.hpp"
+#include "gonzalez_kcentre_rules.hpp"
 
 namespace mlpack{
 namespace kcentre{
@@ -14,19 +15,26 @@ namespace kcentre{
             MetricType& metric):
             datasetOrig(dataset),
             metric(metric),
-            minMaxDistance(0.0)
+            minMaxDistance(0.0),
+            tree(new TreeType(const_cast<MatType&>(datasetOrig))),
     {}
     
     template<typename MetricType , typename MatType>
     GonzalezKcentre<MetricType , MatType>::~GonzalezKcentre()
-    {}
+    {
+        if(tree)
+            delete tree;
+    }
 
     typename<typename MetricType , typename MatType>
     void GonzalezKcentre<MetricType,MatType>::Iterate(
             const MatType& centres,
             MatType& new_centres){
         new_centres.zeros(centres.n_rows,centres.n_cols);
-
+        typedef GonzalezKcentreRules<MetricType, TreeType , MatType> RulesType;
+        RulesType rules(dataset , centres , new_centres , metric);
+        typename TreeType::SingleTreeTranverser<RulesType> traverser(rules);
+        traverser.Traverse(0 , *tree);
     }
 }
 }
